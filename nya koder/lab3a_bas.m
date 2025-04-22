ae8133995b9a9a35b3fddd5a6e47e521750dbe1e
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-04-22 13:34:38 +0200

    Update lab3a_bas.m

--- nya koder/lab3a_bas.m
+++ nya koder/lab3a_bas.m
@@ -30,7 +30,7 @@ function dYdx = crane_ode(x, Y, K0, K1)
     y = Y(1);
     v = Y(2);
     dYdx = [ v;
-            -(K0 - K1*x) * y * (1 + v^2)^(3/2) ];
+    -(K0 - K1*x) * y * (1 + v^2)^(3/2) ];
 end
 
 function [x, Y] = rk4_system(f, x0, Y0, h, L)
