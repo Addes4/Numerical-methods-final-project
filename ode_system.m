c622ba89a3acb27aa35c14a905350743698b0217
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-04-23 13:50:51 +0200

    Delete ode_system.m

--- ode_system.m
+++
@@ -1,9 +0,0 @@
-% Funktion som definierar ODE-systemet
-function dYdx = ode_system(x, Y, K0, K1)
-    % Y(1) = y, Y(2) = y'
-    y = Y(1);
-    v = Y(2);
-    dYdx = zeros(2, 1);
-    dYdx(1) = v;
-    dYdx(2) = - (K0 - K1 * x) * y * (1 + v^2)^(3/2);
-end
