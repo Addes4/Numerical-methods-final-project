36a3a6b4a8edcef7ed0e74bd60e3cd3b9bd76bd0
iamaxeloberg <stadium004@gmail.com>
2025-05-05 14:20:02 +0200

    Update lab3a_bas_short.m

--- förkortade versioner/lab3a_bas_short.m
+++ förkortade versioner/lab3a_bas_short.m
@@ -1,29 +1,38 @@
-%parametrar
+% parametrar
 K0 = 11;
 K1 = 0.2;
 y0 = 0.1;
 s0 = tan(deg2rad(46));
 Y0 = [y0; s0];
 
-%steglängder o intervall
+% steglängder o intervall
 x0 = 0;
 L = 0.5;
 h = 1e-5;  % steg
 h2 = h/2;   % halvasteg
+h4 = h / 4; % fjärdedelssteg
+
 
 % använd den generiska ode_system-funktionen istället för crane_ode
 f = @(x,Y) ode_system(x, Y, K0, K1);
 
-% kör RK4 för h med extern rk4_system
-[~, Yh ]  = rk4_system(f, x0, Y0, h,  L);
-yh = Yh(1,end);
+% Lösningar med olika steglängder 
+[~, Yh]   = rk4_system(f, x0, Y0, h,  L);
+[~, Yh2]  = rk4_system(f, x0, Y0, h2, L);
+[~, Yh4]  = rk4_system(f, x0, Y0, h4, L);
 
-% kör RK4 för h/2
-[~, Yh2] = rk4_system(f, x0, Y0, h2, L);
-yh2 = Yh2(1,end);
+yh   = Yh(1,end);   % y vid x = 0.5 för steglängd h
+yh2  = Yh2(1,end);  % för h/2
+yh4  = Yh4(1,end);  % för h/4
 
-% metodfel via två steglängder
-err_method = abs(yh2 - yh);
+% Fel mellan lösningarna 
+E1 = abs(yh  - yh2);
+E2 = abs(yh2 - yh4);
 
-disp(['y(0.5) = ' num2str(yh2, '%.6f')])
-disp(['metodfel = '    num2str(err_method,  '%.2e')])
+% Konvergensordning
+p_est = log(E1/E2) / log(2);
+
+% Utskrifter
+disp(['y(0.5)        = ' num2str(yh2, '%.8f')])
+disp(['Metodfel      = ' num2str(E2,  '%.2e')])
+disp(['Uppskattad p  = ' num2str(p_est, '%.4f')])
