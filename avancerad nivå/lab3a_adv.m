d86012e569827a60f8fa9b475265ff01f04341f1
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-05-05 14:06:30 +0200

    Update lab3a_adv.m

--- avancerad nivå/lab3a_adv.m
+++ avancerad nivå/lab3a_adv.m
@@ -1,24 +1,30 @@
-K0  = 11;
-K1  = 0.2;
-Y0  = [0.1; tan(deg2rad(46))];   % [y(0); y'(0)]
-xspan = [0, 0.5];
 
-opts_work = odeset('RelTol',1e-12, 'AbsTol',1e-13);  % Arbetslösning
-opts_ref  = odeset('RelTol',1e-14, 'AbsTol',1e-15);  % Referenslösning
+%parametrar
+K0= 11;
+K1 = 0.2;
+Y0 = [0.1; tan(deg2rad(46))]; %startvillkor [y(0); y'(0)]
+xspan = [0, 0.5]; % x-intervall
 
+% solverinställng för metodfel
+opts_work = odeset('RelTol',1e-12,  'AbsTol',1e-13); %arbets­­tolerans för beräknningen
+opts_ref = odeset('RelTol',1e-14,  'AbsTol',1e-15);%referens­­tolerans för referens mot första
+
+%beräkna arbets och referenslösning
 y_work = solve_crane(xspan, Y0, K0, K1, opts_work);
-y_ref  = solve_crane(xspan, Y0, K0, K1, opts_ref);
+y_ref = solve_crane(xspan, Y0, K0, K1, opts_ref); 
 
-p = 4;  % ode45 ≈ RK4 i global ordning
-method_err = abs(y_ref - y_work) / (1 - 2^(-p));
+%felet
+method_err = abs(y_ref - y_work);
 
-disp(['y(0.5)         = ' num2str(y_work, '%.8f')])
-disp(['referensvärde  = ' num2str(y_ref,  '%.8f')])
-disp(['metodfel       = ' num2str(method_err, '%.2e')])
-
+%  funktioner
 function y_end = solve_crane(xspan, Y0, K0, K1, opts)
-    odeFun = @(x, Y) [Y(2); -(K0 - K1*x)*Y(1)*(1 + Y(2)^2)^(3/2)];
+    %kör ODE med ode45 och returnera y då x = xspan(end)
+    odeFun = @(x,Y) [Y(2);-(K0 - K1*x)*Y(1)*(1 + Y(2)^2)^(3/2)];
+    %  lös med ode45:
     [~, Y] = ode45(odeFun, xspan, Y0, opts);
-    y_end = Y(end,1);  % Returnera endast y-komponenten i slutpunkten
+    %tar ebdast y-kompo i slutpunkten:
+    y_end = Y(end,1);
 end
 
+disp(['y(0.5)= ' num2str(y_work,'%.8f')])
+disp(['metodfel= ' num2str(method_err,'%.2e')])
