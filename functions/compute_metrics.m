b1223038cdda29d0d5ca1ee1a41e260dcfb99d55
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-05-05 13:46:40 +0200

    Update compute_metrics.m

--- functions/compute_metrics.m
+++ functions/compute_metrics.m
@@ -1,5 +1,4 @@
 function [slope_end, height_max] = compute_metrics(K0, s0, x0, Y0, h, L, K1)
-% COMPUTE_METRICS Returnerar y'(L) och max y över [x0,L].
     Y0_loc = [Y0(1); s0];
     f = @(x,Y) crane_ode(x, Y, K0, K1);
     [~, Y] = rk4_system(f, x0, Y0_loc, h, L);
