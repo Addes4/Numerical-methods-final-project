b8fa88c8756d70907174684b76b5c9197b6b837c
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-05-05 13:48:16 +0200

    Update rk4_system.m

--- functions/rk4_system.m
+++ functions/rk4_system.m
@@ -1,24 +1,9 @@
 function [x, Y] = rk4_system(f, x0, Y0, h, L)
-% RK4_SYSTEM Generic fourth-order Runge-Kutta integrator for ODE systems
-%   [x, Y] = rk4_system(f, x0, Y0, h, x_end) integrates Y' = f(x,Y) from x0 to x_end
-%   using step size h.
-%
-% INPUT:
-%   f      - function handle @(x, Y) returning column vector dY/dx
-%   x0     - initial value of independent variable
-%   Y0     - initial column vector of dependent variables
-%   h      - integration step size
-%   x_end  - final value of independent variable
-%
-% OUTPUT:
-%   x      - row vector of grid points from x0 to x_end with spacing h
-%   Y      - matrix of size (length(Y0)×length(x)), each column Y(:,i) is solution at x(i)
-
-    % Create grid
+    % grid
     x = x0:h:L;
     N = numel(x);
 
-    % Initialize solution array
+    % lösningsmatris
     m = numel(Y0);
     Y = zeros(m, N);
     Y(:,1) = Y0;
