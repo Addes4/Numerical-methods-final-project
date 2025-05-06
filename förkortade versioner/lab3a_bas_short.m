a0ec6b2f6e11c6ebcf32a1838c4f4b0aad491757
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-05-06 15:32:31 +0200

    Update lab3a_bas_short.m

--- förkortade versioner/lab3a_bas_short.m
+++ förkortade versioner/lab3a_bas_short.m
@@ -5,34 +5,33 @@ y0 = 0.1;
 s0 = tan(deg2rad(46));
 Y0 = [y0; s0];
 
-% steglängder o intervall
+% steglängd o int
 x0 = 0;
 L = 0.5;
-h = 1e-3;  % steg
-h2 = h/2;   % halvasteg
-h4 = h / 4; % fjärdedelssteg
+h = 1e-3;
+h2 = h/2;
+h4 = h / 4;
 
 
-% använd den generiska ode_system-funktionen istället för crane_ode
+%  anropa ode
 f = @(x,Y) ode_system(x, Y, K0, K1);
 
-% Lösningar med olika steglängder 
-[~, Yh]   = rk4_system(f, x0, Y0, h,  L);
-[~, Yh2]  = rk4_system(f, x0, Y0, h2, L);
-[~, Yh4]  = rk4_system(f, x0, Y0, h4, L);
+% lös m olika steglängd 
+[~, Yh] = rk4_system(f, x0, Y0, h,  L);
+[~, Yh2] = rk4_system(f, x0, Y0, h2, L);
+[~, Yh4] = rk4_system(f, x0, Y0, h4, L);
 
-yh   = Yh(1,end);   % y vid x = 0.5 för steglängd h
-yh2  = Yh2(1,end);  % för h/2
-yh4  = Yh4(1,end);  % för h/4
+yh = Yh(1,end);   % y vid x = 0.5 för steglängd h
+yh2 = Yh2(1,end); 
+yh4  = Yh4(1,end); 
 
-% Fel mellan lösningarna 
+% fel mellan lösningarna 
 E1 = abs(yh  - yh2);
 E2 = abs(yh2 - yh4);
 
-% Konvergensordning
+% konv.ordn
 p_est = log(E1/E2) / log(2);
 
-% Utskrifter
-disp(['y(0.5)        = ' num2str(yh2, '%.8f')])
-disp(['Metodfel      = ' num2str(E1,  '%.2e')])
-disp(['Uppskattad p  = ' num2str(p_est, '%.4f')])
+disp(['y(0.5)  = ' num2str(yh2, '%.8f')])
+disp(['metodfelet = ' num2str(E1,  '%.2e')])
+disp([' p  = ' num2str(p_est, '%.4f')])
